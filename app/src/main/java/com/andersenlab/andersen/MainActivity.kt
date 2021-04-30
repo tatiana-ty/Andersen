b2034307fda8881f588459ac5745d15d3e015b39
Tatiana Tiurikova <tatiana.tiurikova.a@gmail.com>
2021-04-30 20:36:12 +0300

    Toast

--- app/src/main/java/com/andersenlab/andersen/MainActivity.kt
+++ app/src/main/java/com/andersenlab/andersen/MainActivity.kt
@@ -2,11 +2,13 @@ package com.andersenlab.andersen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         println("Hello World!")
+        makeText(baseContext, "Hello", LENGTH_LONG).show()
     }
 }
